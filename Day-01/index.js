02b4beb0955e890458eba37c720e7fcf0c41d452
chaithanya-9 <chaithanyayadav09@gmail.com>
2026-05-04 16:19:02 +0530

    Day-01 class practiced arithmetic operators, logical operators, built-in string functions

---
+++ Day-01/index.js
@@ -0,0 +1,38 @@
+let a = 10;
+let b = 20;
+let name = "Java Script";
+
+// Arithmatic Operators
+console.log("Addition:", a + b); // '+' will add two numbers and if oneside is string then it concatinates 
+console.log("Subtraction:", a - b); // '-' will subtract right variable's value from left variable's value
+console.log("Multiplication:", a * b); // '*' will multiply variables values
+console.log("Division:", a / b); // '/' will divide left variable's value by right variable's value
+console.log("Modulus:", a % b); // '%' will return remainder after divisiion
+console.log("Exponential:", a ** b); // '**' will raises left variable's value to the power of right variable's value
+
+// Logical Operators
+console.log("a > b:", a > b); // returns false as a not greater than b
+console.log("a < b:", a < b); // returns true as a less than b
+console.log("a == b:", a == b); // returns false as a is not equal to b ('===' is strict equal which checks for value and datatype)
+console.log("a != b:", a != b); // returns true as a is not equal to b 
+console.log("AND:", a > 20 && b < a); // '&&' will return true if all the conditions are true 
+console.log("OR:", a > 20 || b < 30); // '||' will return true if any of the conditions is true
+console.log("NOT:", !true); // '!' will flip the boolean value (true to false, false to true)
+
+// String Functions
+console.log("Length:", name.length); // returns the number of characters present in the string
+console.log("Upper:", name.toUpperCase()); // converts string to upper case
+console.log("Lower:", name.toLowerCase()); // converts string to lower case
+console.log("Substring:", name.substring(0, 4)); // extracts characters from start index up to end index (but not includes end index character)
+
+
+
+
+
+
+
+
+
+
+
+
