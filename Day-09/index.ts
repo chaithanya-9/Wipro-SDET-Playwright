3360956b53208388511071b377f81be093d59f44
chaithanya-9 <chaithanyayadav09@gmail.com>
2026-05-11 15:39:53 +0530

    learn generics in functions,class,interface

--- Day-09/index.ts
+++ Day-09/index.ts
@@ -4,9 +4,94 @@ console.log(name);
 let name1: string = "nobody"; // shoudld only assing strings
 console.log(name1);
 
+let name2: number | string;
+name2 = 100; // can assign number
+name2 = "str"; // can assign string
+
 // this function takes numbers as arguments and returns string
 function addition(num1: number, num2: number): string {
     return "sum: " + (num1 + num2);
 }
 
 console.log(addition(10, 20));
+
+interface Animal {
+    id: number;
+    type: string;
+    name: string;
+    friendly?: Friendly; // use enum, options(Yes/No) and ? denotes the property is optional
+}
+enum Friendly {
+    Yes = "yes",
+    No = "no"
+}
+const dog: Animal = {
+    id: 1,
+    type: "dog",
+    name: "Jessie",
+    friendly: Friendly.Yes
+}
+const cat: Animal = {
+    id: 1,
+    type: "cat",
+    name: "catsik"
+}
+console.log(dog, "\n", cat);
+
+//tuple => is like arrays but fixed size 
+const tuple: [number, number] = [10, 20];
+console.log("tuple:", tuple);
+
+// arrays
+const arr: string[] = ["abc", "def"];
+console.log("arr", arr);
+
+//Generics -> its reusable code which will work for any type
+
+//generics in function
+
+function generic<T>(val1: T): T {
+    return val1;
+}
+
+const numFunction = generic<number>(10);
+const stringFunction = generic<string>("str");
+const booleanFunction = generic<boolean>(true);
+
+console.log(`numFunction: ${numFunction}\nstringFunction: ${stringFunction}\nbooleanFunction: ${booleanFunction}`);
+
+//generic interface
+interface GenericInterface<T> {
+    content: T;
+}
+
+const fruits: GenericInterface<string> = {
+    content: "apples"
+}
+
+const numbers: GenericInterface<number> = {
+    content: 10
+}
+
+console.log(`fruits: ${fruits.content}\nnumbers: ${numbers.content}`);
+
+// generic class
+
+class Storage<T> {
+    private items: T[] = [];
+
+    addItems(item: T): void {
+        this.items.push(item);
+    }
+
+    getItem(index: number): T {
+        return this.items.at(index)!;
+    }
+}
+
+const names = new Storage<string>();
+
+names.addItems("abc");
+names.addItems("def");
+
+console.log(`items[0]: ${names.getItem(0)}\nitems[1]: ${names.getItem(1)}\nitems[2]:${names.getItem(2)}`);
