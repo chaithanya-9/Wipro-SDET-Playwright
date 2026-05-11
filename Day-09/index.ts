d31021ad84b673da0fa9392328b0b31fee743b71
chaithanya-9 <chaithanyayadav09@gmail.com>
2026-05-11 12:45:54 +0530

    learn typescript basics, types and typed function

--- Day-09/index.ts
+++ Day-09/index.ts
@@ -1,2 +1,12 @@
 let name = "nobody";
 console.log(name);
+
+let name1: string = "nobody"; // shoudld only assing strings
+console.log(name1);
+
+// this function takes numbers as arguments and returns string
+function addition(num1: number, num2: number): string {
+    return "sum: " + (num1 + num2);
+}
+
+console.log(addition(10, 20));
