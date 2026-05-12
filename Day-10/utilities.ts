ab4b264c1beda442e9b7b25cfa6fae771f4484e0
chaithanya-9 <chaithanyayadav09@gmail.com>
2026-05-12 16:42:52 +0530

    add comments or each and learn DeepPartial

--- Day-10/utilities.ts
+++ Day-10/utilities.ts
@@ -37,7 +37,23 @@ type PartialUserWithoutAge = Partial<Omit<User, "age">>;
 
 // create custome DeepPartial<> type
 type DeepPartial<T> = {
-    [P in keyof T]?:
-    T[P] extends (infer U)[] ? DeepPartial<U>[] :
-    T[P] extends Object ? T[P] : T[P]
-}
+    [P in keyof T]?: // makes every key optional present in object 
+    T[P] extends (infer U)[] ? DeepPartial<U>[] : // takes that every key and checks if its an array or not, if its an array then it will tka that array and makes its element as DeepPartial and then again makes that array of that DeepPartial type
+    T[P] extends Object ? T[P] : T[P] //if its not an array then it will check if its an object or not, if its an object then it will make that object as DeepPartial and if its not an object then it will return that type as it is because we only want to make objects as DeepPartial and not primitive types  
+} // uses ternary operator to check if its an array or object or primitive type
+
+const user = {
+    id: 21,
+    name: "Aryan",
+    age: 25
+} as const;
+// as const makes entire object as readonly
+type User1 = typeof user; // this will be having {readonly id:21, readonly name:"Aryan", readonly age:25} because of as const
+type PartialUser1 = DeepPartial<User1>; // this will be having {id?:21, name?:"Aryan", age?:25} because of DeepPartial and as const makes it readonly but we are making it optional so it will remove readonly and make it optional
+
+// or we can use built-in Readonly utility type to make entire object as readonly
+type ReadonlyUser1 = Readonly<User1>; // this will be having {readonly id:21, readonly name:"Aryan", readonly age:25} because of Readonly utility type
+
+// we can make arrays as readonly using ReadonlyArray utility type
+type ReadonlyUserArray = ReadonlyArray<User1>;
+
