42ef364da65f3009537d6d038f81f64a7d32b89f
chaithanya-9 <chaithanyayadav09@gmail.com>
2026-05-14 18:59:08 +0530

    add negative password test

--- Day-12/tests/pracTestAuto.spec.js
+++ Day-12/tests/pracTestAuto.spec.js
@@ -22,3 +22,12 @@ test('negative username test', async ({ page }) => {
     await expect(page.locator('#error')).toHaveText('Your username is invalid!');
 });
 
+test('Negative password test', async ({ page }) => {
+    await page.goto('https://practicetestautomation.com/practice-test-login/');
+    await page.locator('#username').fill('student');
+    await page.locator('#password').fill('incorrectPass');
+    await page.locator('#submit').click();
+
+    await expect(page.locator('#error')).toBeVisible();
+    await expect(page.locator('#error')).toHaveText('Your password is invalid!');
+})
