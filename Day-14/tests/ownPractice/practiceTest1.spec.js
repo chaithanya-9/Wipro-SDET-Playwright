6a457402606269b0418ea810656af987b139f017
chaithanya-9 <chaithanyayadav09@gmail.com>
2026-05-17 11:07:36 +0530

    practice test 2

--- Day-14/tests/ownPractice/practiceTest1.spec.js
+++ Day-14/tests/ownPractice/practiceTest1.spec.js
@@ -9,3 +9,10 @@ test('practice test 1', async ({ page }) => {
     await expect(page).toHaveURL('https://demo.applitools.com/app.html');
 })
 
+test('practice test 2', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByPlaceholder('Username').fill('Admin');
+    await page.getByPlaceholder('Password').fill('admin123');
+    await page.getByRole('button', { name: /Login/ }).click();
+    await expect(page).toHaveURL(' https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+})
